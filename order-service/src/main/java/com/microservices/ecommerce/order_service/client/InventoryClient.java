1871d8a739bf93ced16d77a149af603ce30c6832
aadish233 <jainaadish881@gmail.com>
2025-07-30 19:41:22 +0530

    Added OAuth2

--- order-service/src/main/java/com/microservices/ecommerce/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/microservices/ecommerce/order_service/client/InventoryClient.java
@@ -1,6 +1,5 @@
 package com.microservices.ecommerce.order_service.client;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
